9b7ed3939a8e46af47a29c8c9a2a0bc9ca9ce3d2
rbgir1113-dot <rbgir1113@gmail.com>
2026-05-06 12:28:52 +0900

    layout Edit

--- src/pages/layout/AuthLayout.jsx
+++ src/pages/layout/AuthLayout.jsx
@@ -22,28 +22,28 @@ const AuthLayout = () => {
           
           {/* 학습 페이지 이동 */}
           <button>
-            <Link to="/study" style={styles.studyNav}>
+            <Link to="/study" style={styles.contentNav}>
               학습
             </Link>
           </button>
 
           {/* 시험 페이지 이동 */}
           <button>
-            <Link to="/exam/info/notice" style={styles.examNav}>
+            <Link to="/exam/info/notice" style={styles.contentNav}>
                 시험
             </Link>
           </button>
 
           {/* 커뮤니티 페이지 이동 */}
           <button >
-            <Link to="/community" style={styles.communityNav}>
+            <Link to="/community" style={styles.contentNav}>
               커뮤니티
             </Link>
           </button>
 
           {/* 고객지원 페이지 이동 */}
           <button>
-            <Link to="/customservice" style={styles.customserviceNav}>
+            <Link to="/customservice" style={styles.contentNav}>
               고객지원
             </Link>
           </button>
@@ -56,10 +56,10 @@ const AuthLayout = () => {
             alignItems:'center'
           }}>
           <div style={{ display: 'flex', gap: '24px', alignItems: 'center' }}>
-            <button style={styles.loginNav}>
-              <Link to="/mypage" style={styles.login}>마이페이지</Link>
+            <button style={styles.rightNav}>
+              <Link to="/mypage" style={styles.rigthBorderNav}>마이페이지</Link>
             </button>
-              <button style={styles.joinNav} onClick={handleLogout}>로그아웃</button>
+              <button style={styles.rigthBackGroundNav} onClick={handleLogout}>로그아웃</button>
           </div>
         </nav>
       </header>
