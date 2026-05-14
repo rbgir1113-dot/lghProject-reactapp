438279b645f28b091ecbb6c06d86b8eaf75f5d8f
scminy0527-rgb <scminy0527@gmail.com>
2026-05-14 19:40:04 +0900

    refactor: PageCount.jsx 스타일 규칙

--- src/pages/community/post/postComponents/PageCount.jsx
+++ src/pages/community/post/postComponents/PageCount.jsx
@@ -1,53 +1,25 @@
 import React from "react";
-import styled, { css } from "styled-components";
-import theme from "../../../../styles/theme";
-import { h11Bold, h11Regular } from "../../../../styles/common";
+import { PageButtonRow, PageButton, HighlightButton } from "./pageCountStyle";
 
-const PageButtonRow = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 10px;
-  margin-bottom: 80px;
-`;
-
-const buttonStyle = css`
-  width: 36px;
-  height: 36px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const PageButton = styled.div`
-  ${buttonStyle}
-  border: 2px solid ${theme.GRAYSCALE[8]};
-  ${h11Regular}
-`;
-
-const HighlightButton = styled.div`
-  ${buttonStyle}
-  border: 2px solid ${theme.PALETTE.primary.main};
-  background: ${theme.PALETTE.primary.main};
-  ${h11Bold}
-  color: ${theme.PALETTE.white};
-`;
+const S = {
+  PageButtonRow,
+  PageButton,
+  HighlightButton,
+};
 
 const PageCount = ({ totalPages = 1, currentPage = 1, onPageChange }) => {
   return (
-    <PageButtonRow>
+    <S.PageButtonRow>
       {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) =>
         page === currentPage ? (
-          <HighlightButton key={page}>{page}</HighlightButton>
+          <S.HighlightButton key={page}>{page}</S.HighlightButton>
         ) : (
-          <PageButton key={page} onClick={() => onPageChange(page)}>
+          <S.PageButton key={page} onClick={() => onPageChange(page)}>
             {page}
-          </PageButton>
+          </S.PageButton>
         ),
       )}
-    </PageButtonRow>
+    </S.PageButtonRow>
   );
 };
 
